640831f30dbbd09c6150d0ae107bd2aff9671dc8
Mark McCracken <mark.mccracken@miceanddice.com>
2018-02-19 23:55:35 +0000

    Super basic setup

--- run-process.ts
+++ run-process.ts
@@ -1,13 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const moment = require("moment");
-const fs = require("fs");
+import * as moment from "moment";
+import * as fs from "fs";
 const id = process.pid;
 const processType = `run_process`;
 const version = 'alpha';
+
 console.log(`Running process`);
 console.log(`Process ID: ${id}`);
+
 console.log(moment().format("YYYY-MM-DD Do HH:mm:ss"));
+
 console.log(version);
+
 fs.writeFileSync(`./logs/${processType}/${process.pid}`, `PID: ${id}\t${processType}\t${version}\t${new Date()}\n`);
+
 console.log(`all done`);
